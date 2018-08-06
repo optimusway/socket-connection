e06a34946a76b6300abd291cc9d04b90c902c0c3
Sergey <sschegolev87@gmail.com>
2018-08-06 09:10:55 +0300

    Change socket's interface

--- src/socket.ts
+++ src/socket.ts
@@ -1,44 +1,29 @@
 export interface IProxy {
-  connect: () => void;
-  close: () => void;
-  subscribe: (subscriptionName: string, callback: any) => void;
-  unsubscribe: (subscriptionName: string) => void;
-  unsubscribeFromAll?: () => void;
-  subscribeToAll?: () => void;
-  getAllSubscriptions?: () => any;
+  connect: () => Promise<void>;
+  close: () => Promise<void>;
+  send: (options: any) => Promise<void>;
   getSubscription?: (subscriptionName: string) => any;
   isConnected?: () => boolean;
 }
 
 export interface ISocket {
-  connect: () => void;
-  close: () => void;
-  subscribe: (subscriptionName: string, callback: any) => void;
-  unsubscribe: (subscriptionName: string) => void;
-  unsubscribeFromAll: () => void;
-  subscribeToAll: () => void;
-  getAllSubscriptions: () => any;
-  getSubscription: (subscriptionName: string) => any;
+  connect: () => Promise<void>;
+  close: () => Promise<void>;
+  send: (options: any) => Promise<void>;
   getCurrentProxy: () => IProxy;
   isSocketConnected: () => boolean;
 }
 
 export class Socket implements ISocket {
-
   proxy: IProxy;
 
   constructor(proxy: IProxy) {
-    this.proxy = proxy
+    this.proxy = proxy;
   }
 
-  connect = async() => await this.proxy.connect();
-  close = async() => await this.proxy.close();
-  subscribe = async(subscriptionName: string, callback: any) => await this.proxy.subscribe(subscriptionName, callback);
-  unsubscribe = async(subscriptionName: string) => await this.proxy.unsubscribe(subscriptionName);
-  subscribeToAll = async() => await this.proxy.subscribeToAll!();
-  unsubscribeFromAll = async() => await this.proxy.unsubscribeFromAll!();
-  getAllSubscriptions = () => this.proxy.getAllSubscriptions!();
-  getSubscription = (subscriptionName: string) => this.proxy.getSubscription!(subscriptionName);
+  connect = async () => await this.proxy.connect();
+  close = async () => await this.proxy.close();
+  send = async (options: any) => await this.proxy.send(options);
   getCurrentProxy = () => this.proxy;
   isSocketConnected = () => this.proxy.isConnected!();
 }
